c136355a0895c3507533d3b53df45697c89846d7
Andreas <andreasjr77@outlook.com>
2024-04-07 11:24:47 +0300

    app menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
-import { PersonTableComponent } from './components/person-table/person-table.component';
-import { Person } from './shared/interfaces/person';
-import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterOutlet,
-    PersonTableComponent,
-    EventBindExampleComponent,
-  ],
+  imports: [RouterLink, RouterOutlet, ListGroupMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
- 
-}
+export class AppComponent {}
---
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MenuItem } from 'src/app/shared/interfaces/menu-item';
+
+@Component({
+  selector: 'app-list-group-menu',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './list-group-menu.component.html',
+  styleUrl: './list-group-menu.component.css',
+})
+export class ListGroupMenuComponent {
+  menu: MenuItem[] = [
+    { text: 'Component Input Example', routerLink: 'component-input-example' },
+    { text: '@for Directive Example', routerLink: 'for-directive-example' },
+    { text: 'Event Bind Example', routerLink: 'event-bind-example' },
+  ];
+}
